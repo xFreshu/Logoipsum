fac135d765887f0168c8a7cfc043f2b21b12ef6b
xFreshu <lprzybyszcontact@gmail.com>
2022-05-31 12:46:08 +0200

    add file structure, fonts, Router to the app, dev dependencies for styled-components

---
+++ src/pages/App/App.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  background-color: #F6FFF8;
+  font-family: 'Roboto', sans-serif;
+  height: 100vh;
+  width: 100%;
+`
---
+++ src/pages/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Wrapper } from './App.styles'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Wrapper>Hello World</Wrapper>
+    </>
+  )
+}
+
+export default App
